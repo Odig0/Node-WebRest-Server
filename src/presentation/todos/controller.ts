6816aa1349ed243076b34fc2dfd4e1bbeec8bc33
Diego <diegoguzman366@gmail.com>
2024-03-20 12:04:54 -0400

    Se agrega la base de datos con postgres y prisma

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../data/postgres';
-
-
+import { CreateTodoDto } from '../../domain/dtos';
+import { UpdateTodoDto } from '../../domain/dtos';
 
 
 export class TodosController {
@@ -31,39 +31,34 @@ export class TodosController {
   };
 
   public createTodo = async( req: Request, res: Response ) => {
-    const { text } = req.body;
-    if ( !text ) return res.status( 400 ).json( { error: 'Text property is required' } );
+    const [error, createTodoDto] = CreateTodoDto.create( req.body)
+    if(error) return res.status(400).json({error});
     
     const todo = await prisma.todo.create({
-      data: {text: text}
+      data: createTodoDto!
     })
 
     res.json( todo );
 
   };
 
-  public updateTodo = async ( req: Request, res: Response ) => {
+  public updateTodo = async( req: Request, res: Response ) => {
     const id = +req.params.id;
-    if ( isNaN( id ) ) return res.status( 400 ).json( { error: 'ID argument is not a number' } );
+    const [error, updateTodoDto] = UpdateTodoDto.create({...req.body, id});
+    if ( error ) return res.status(400).json({ error });
     
-    const todo = await prisma.todo.findFirst( {
-      where: { id: id}
+    const todo = await prisma.todo.findFirst({
+      where: { id }
     });
 
     if ( !todo ) return res.status( 404 ).json( { error: `Todo with id ${ id } not found` } );
 
-    const { text, completedAt } = req.body;
-    
-    const updateTodo = await prisma.todo.update({
-      where: { id},
-      data: {
-         text, 
-         completedAt: (completedAt) ? new Date(completedAt) : null 
-        }
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: updateTodoDto!.values
     });
-    
-
-    res.json( updateTodo );
+  
+    res.json( updatedTodo );
 
   }
 
@@ -87,6 +82,5 @@ export class TodosController {
 
   }
   
-
 
 }
